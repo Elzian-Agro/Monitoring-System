516778b47c93b4ecb02d30a7239f8717c289e917
Musarraf <muhammedmusarraf@gmail.com>
2023-12-29 04:28:37 +0530

    Added small hover animations and pie icon

--- src/components/common/sidebar/index.jsx
+++ src/components/common/sidebar/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { UserIcon, ArrowTrendingUpIcon, Cog6ToothIcon, PlusIcon, ArrowUpTrayIcon, XMarkIcon, Squares2X2Icon } from '@heroicons/react/24/outline'
+import { UserIcon, ArrowTrendingUpIcon, Cog6ToothIcon, PlusIcon, ArrowUpTrayIcon, XMarkIcon, Squares2X2Icon, ChartPieIcon } from '@heroicons/react/24/outline'
 import logo from "assets/images/logo.png"
 
 import { Link, NavLink } from "react-router-dom";
@@ -10,7 +10,7 @@ const Sidebar = () => {
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white bg-green-500 text-md m-2";
   const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-black dark:hover:text-black hover:bg-gray-100 m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-black dark:hover:text-black hover:bg-gray-100 m-2 hover:translate-x-2 duration-300";
 
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-2 ">
@@ -22,7 +22,7 @@ const Sidebar = () => {
             <Link
               to="/dashboard"
               onClick={() => {}}
-              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight text-slate-900"
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight text-slate-900 "
             >
               <img className="w-20" src={logo} alt="Elzian Agro logo"/>
               <span>Elzian Agro</span>
@@ -41,6 +41,7 @@ const Sidebar = () => {
               to="/dashboard"
               onClick={() => {}}
               className={({ isActive }) => (isActive ? activeLink : normalLink)}
+              
             >
               <Squares2X2Icon className="h-6 w-6"/>
               Dashboard
@@ -94,6 +95,7 @@ const Sidebar = () => {
               onClick={() => {}}
               className={({ isActive }) => (isActive ? activeLink : normalLink)}
             >
+            <ChartPieIcon className="h-6 w-6"/>
               Pie
             </NavLink>
 
