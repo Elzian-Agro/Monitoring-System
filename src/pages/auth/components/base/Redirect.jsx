baba92ba0c90dc0adaef4c30bd2f7235a47ec94d
Abdullah Anaz <abdullahanaz@yahoo.com>
2023-12-29 07:30:43 +0530

    ADD: Redirect Functionality

---
+++ src/pages/auth/components/base/Redirect.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+
+function Redirect({ setPage, Icon, message, type }) {
+  const [timer, setTimer] = useState(10);
+
+  useEffect(() => {
+      let interval;
+      if (timer > 0) {
+        interval = setInterval(() => {
+          setTimer((prevTimer) => prevTimer - 1);
+        }, 1000);
+      } else if (timer === 0) {
+        clearInterval(interval);
+        setPage("Login");
+      }
+      return () => clearInterval(interval);
+  }, [timer]);
+
+  return (
+    <div className="flex-1 flex items-center flex-col lg:justify-center h-full w-full">
+      <Icon
+        className={`w-[50px] ${
+          type === "warning" ? "text-red-300" : "text-[#0B802199]"
+        }`}
+      />
+      <p
+        className={`font-zenkaku font-bold text-[24px] ${
+          type === "warning" ? "text-red-300" : "text-[#0B802199]"
+        }`}
+      >
+        {message}
+      </p>
+      <p
+        className={`font-zenkaku font-light text-[14px] ${
+          type === "warning" ? "text-red-300" : "text-[#0B802199]"
+        }`}
+      >
+        You will be redirected in {timer} seconds
+      </p>
+    </div>
+  );
+}
+
+export default Redirect;
--- src/pages/auth/reset-password/index.jsx
+++ src/pages/auth/reset-password/index.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { ArrowLeftIcon, LockClosedIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowLeftIcon,
+  LockClosedIcon,
+  ExclamationTriangleIcon,
+  CheckCircleIcon
+} from "@heroicons/react/24/outline";
 import Button from "pages/auth/components/base/Button";
 import TextBox from "pages/auth/components/base/TextBox";
 import ErrorMessage from "pages/auth/components/base/ErrorMessage";
@@ -9,6 +14,7 @@ import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { updateEmail } from "../slice/emailSlice";
 import { sha256 } from "js-sha256";
+import Redirect from "pages/auth/components/base/Redirect";
 
 function ResetPassword({ setPage }) {
   const [tempPass, setTempPass] = useState("");
@@ -19,6 +25,7 @@ function ResetPassword({ setPage }) {
   const hashedEmail = useSelector((state) => state.email.value);
   const dispatch = useDispatch();
   const [timer, setTimer] = useState(60);
+  const [success, setSuccess] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -48,7 +55,7 @@ function ResetPassword({ setPage }) {
         setError(null);
         dispatch(updateEmail(null));
         setIsLoading(false);
-        setPage("Login");
+        setSuccess(true);
       })
       .catch((error) => {
         setIsLoading(false);
@@ -60,7 +67,12 @@ function ResetPassword({ setPage }) {
       });
   };
 
-   useEffect(() => {
+  const handleGoBack = () => {
+    dispatch(updateEmail(null));
+    setPage("Login");
+  };
+
+  useEffect(() => {
     let interval;
     if (timer > 0) {
       interval = setInterval(() => {
@@ -72,7 +84,6 @@ function ResetPassword({ setPage }) {
     return () => clearInterval(interval);
   }, [timer]);
 
-  
   const handleResendEmail = () => {
     axios
       .post("url")
@@ -80,77 +91,99 @@ function ResetPassword({ setPage }) {
         setTimer(60);
       })
       .catch((error) => {
-        alert(`${error.response.status}: An Unexpected Error Occured!`)
+        alert(`${error.response.status}: An Unexpected Error Occured!`);
       });
   };
 
   return (
     <div className="flex flex-col h-full w-full gap-5 md:gap-0 items-start">
       <button
-        onClick={() => setPage("Login")}
+        onClick={handleGoBack}
         className="group w-[100px] flex items-center"
       >
         <ArrowLeftIcon className="w-[20px] group-hover:ml-[-20px] transition-all" />
         <p className="flex-1 font-zenkaku text-[12px]">Go Back</p>
       </button>
 
-      <div className="flex-1 flex items-center flex-col lg:justify-center h-full w-full">
-        <h1 className="font-zenkaku font-black text-[#212121] text-[18px] sm:text-[26px] leading-5 sm:leading-10">
-          RESET PASSWORD
-        </h1>
-        <p className="font-zenkaku font-normal text-center text-[#999] text-[10px] sm:text-[16px] leading-5 xxs:leading-10">
-          TEMPORARY PASSWORD HAS BEEN SENT TO YOUR EMAIL
-        </p>
+      {hashedEmail && !success && (
+        <div className="flex-1 flex items-center flex-col lg:justify-center h-full w-full">
+          <h1 className="font-zenkaku font-black text-[#212121] text-[18px] sm:text-[26px] leading-5 sm:leading-10">
+            RESET PASSWORD
+          </h1>
+          <p className="font-zenkaku font-normal text-center text-[#999] text-[10px] sm:text-[16px] leading-5 xxs:leading-10">
+            TEMPORARY PASSWORD HAS BEEN SENT TO YOUR EMAIL
+          </p>
 
-        <form
-          className="flex flex-col items-center p-2 xs:p-4 gap-4 w-full"
-          onSubmit={handleSubmit}
-        >
-          <TextBox
-            placeholder="Enter  Temporary Password"
-            label="Temporary Password"
-            type="password"
-            Icon={LockClosedIcon}
-            value={tempPass}
-            setValue={setTempPass}
-          />
+          <form
+            className="flex flex-col items-center p-2 xs:p-4 gap-4 w-full"
+            onSubmit={handleSubmit}
+          >
+            <TextBox
+              placeholder="Enter  Temporary Password"
+              label="Temporary Password"
+              type="password"
+              Icon={LockClosedIcon}
+              value={tempPass}
+              setValue={setTempPass}
+            />
 
-          <TextBox
-            placeholder="Enter New Password"
-            label="New Password"
-            type="password"
-            Icon={LockClosedIcon}
-            value={newPass}
-            setValue={setNewPass}
-          />
+            <TextBox
+              placeholder="Enter New Password"
+              label="New Password"
+              type="password"
+              Icon={LockClosedIcon}
+              value={newPass}
+              setValue={setNewPass}
+            />
 
-          <TextBox
-            placeholder="Enter New Password Again"
-            label="Confirm Password "
-            type="password"
-            Icon={LockClosedIcon}
-            value={confirmPass}
-            setValue={setConfirmPass}
-          />
+            <TextBox
+              placeholder="Enter New Password Again"
+              label="Confirm Password "
+              type="password"
+              Icon={LockClosedIcon}
+              value={confirmPass}
+              setValue={setConfirmPass}
+            />
 
-          {error && <ErrorMessage message={error} />}
+            {error && <ErrorMessage message={error} />}
 
-          <Button text="Continue" disabled={isLoading} />
-        </form>
+            <Button text="Continue" disabled={isLoading} />
+          </form>
 
-        <div>
-          {timer > 0 ? (
-            <p className="font-zenkaku font-light text-[12px] text-center">Email Sent! Didn't Recieve? Resend Email in {timer} Seconds</p>
-          ) : (
-            <button
-              onClick={handleResendEmail}
-              className="text-blue-500 hover:text-blue-700 font-zenkaku"
-            >
-              Resend Email
-            </button>
-          )}
+          <div>
+            {timer > 0 ? (
+              <p className="font-zenkaku font-light text-[12px] text-center">
+                Email Sent! Didn't Recieve? Resend Email in {timer} Seconds
+              </p>
+            ) : (
+              <button
+                onClick={handleResendEmail}
+                className="text-blue-500 hover:text-blue-700 font-zenkaku"
+              >
+                Resend Email
+              </button>
+            )}
+          </div>
         </div>
-      </div>
+      )}
+
+      {(!hashedEmail && !success) && (
+        <Redirect
+          setPage={setPage}
+          Icon={ExclamationTriangleIcon}
+          message={"Unauthorized Access"}
+          type={"warning"}
+        />
+      )}
+
+      {success && (
+        <Redirect
+          setPage={setPage}
+          Icon={CheckCircleIcon}
+          message={"Password Reset Successfully"}
+          type={"success"}
+        />
+      )}
     </div>
   );
 }
