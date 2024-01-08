7ba2ed233eb1c7dcb07794a2c145628b71339ec5
Musarraf <muhammedmusarraf@gmail.com>
2024-01-08 23:55:58 +0530

    Created 2 new functions inside dashboard index.js to reduce the amount of code and implement clean code in some other files as well

--- src/components/common/sidebar/index.jsx
+++ src/components/common/sidebar/index.jsx
@@ -19,90 +19,81 @@ const Sidebar = () => {
   const dispatch = useDispatch();
   const activeMenu = useSelector(selectActiveMenu);
 
-  // This function for check if the active menu is 'open'
+  // This function for check if the active menu is 'open' if so will return true
   const isOpenMenu = () => activeMenu === "open";
 
   const currentYear = new Date().getFullYear();
 
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white bg-green-500 text-md m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white bg-green-500 text-md m-2";
   const normalLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-black dark:hover:text-black hover:bg-gray-100 m-2 hover:translate-x-2 duration-300";
 
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-2 ">
-      {activeMenu && (
-        <>
-          <div className="flex justify-between items-center">
-            <Link
-              to="/dashboard"
-              onClick={() => {
-                dispatch(setActiveMenu(false));
-              }}
-              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight text-slate-900 "
-            >
-              <img className="w-14" src={logo} alt="Elzian Agro logo" />
-              {isOpenMenu() && <span>Elzian Agro</span>}
-            </Link>
-            {isOpenMenu() && (
-              <button
-                type="button"
-                onClick={() => {
-                  dispatch(setActiveMenu("onlyIcon"));
-                }}
-                // style={{ color: currentColor }}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
-              >
-                <ArrowLeftCircleIcon className="h-6 w-6" />
-              </button>
-            )}
-          </div>
-          <div className="sidebar-items mt-10">
-            {sidebarLinks.map((link) => (
-              <NavLink
-                key={link.to}
-                to={link.to}
-                onClick={() => {}}
-                className={({ isActive }) =>
-                  isActive ? activeLink : normalLink
-                }
-              >
-                <link.icon className="h-6 w-6" />
-                {isOpenMenu() && link.text}
-              </NavLink>
-            ))}
+      <div className="flex justify-between items-center">
+        <Link
+          to="/dashboard"
+          onClick={() => {
+            dispatch(setActiveMenu(false));
+          }}
+          className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight text-slate-900 "
+        >
+          <img className="w-14" src={logo} alt="Elzian Agro logo" />
+          {isOpenMenu() && <span>Elzian Agro</span>}
+        </Link>
+        {isOpenMenu() && (
+          <button
+            type="button"
+            onClick={() => {
+              dispatch(setActiveMenu("onlyIcon"));
+            }}
+            // style={{ color: currentColor }}
+            className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+          >
+            <ArrowLeftCircleIcon className="h-6 w-6" />
+          </button>
+        )}
+      </div>
+      <div className="sidebar-items mt-10">
+        {sidebarLinks.map((link) => (
+          <NavLink
+            key={link.to}
+            to={link.to}
+            className={({ isActive }) => (isActive ? activeLink : normalLink)}
+          >
+            <link.icon className="h-6 w-6" />
+            {isOpenMenu() && link.text}
+          </NavLink>
+        ))}
 
-            {/* Title Charts */}
-            <div className="Title">
-              <p className=" text-gray-400 m-3 mt-4 uppercase">Charts</p>
-            </div>
+        {/* Title Charts */}
+        <div className="Title">
+          <p className=" text-gray-400 m-3 mt-4 uppercase">Charts</p>
+        </div>
 
-            <NavLink
-              to="/pie"
-              onClick={() => {}}
-              className={({ isActive }) => (isActive ? activeLink : normalLink)}
-            >
-              <ChartPieIcon className="h-6 w-6" />
-              {isOpenMenu() && "Pie"}
-            </NavLink>
+        <NavLink
+          to="/pie"
+          className={({ isActive }) => (isActive ? activeLink : normalLink)}
+        >
+          <ChartPieIcon className="h-6 w-6" />
+          {isOpenMenu() && "Pie"}
+        </NavLink>
 
-            {/* Logout */}
-            <NavLink
-              to="/"
-              onClick={() => {}}
-              className="flex mt-12 items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-black dark:hover:text-black hover:bg-red-500 m-2 duration-300"
-            >
-              <ArrowUpTrayIcon className="h-6 w-6 rotate-90" />
-              {isOpenMenu() && "Logout"}
-            </NavLink>
-          </div>
-          {isOpenMenu() && (
-            <footer className="text-gray-400 text-xs text-center">
-              <p>Copyright {currentYear} Elzian Agro.</p>
-              <p>All Rights Reserved</p>
-            </footer>
-          )}
-        </>
+        {/* Logout */}
+        <NavLink
+          to="/"
+          className="flex mt-12 items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-black dark:hover:text-black hover:bg-red-500 m-2 duration-300"
+        >
+          <ArrowUpTrayIcon className="h-6 w-6 rotate-90" />
+          {isOpenMenu() && "Logout"}
+        </NavLink>
+      </div>
+      {isOpenMenu() && (
+        <footer className="text-gray-400 text-xs text-center mt-12">
+          <p>Copyright {currentYear} Elzian Agro.</p>
+          <p>All Rights Reserved</p>
+        </footer>
       )}
     </div>
   );
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -7,33 +7,41 @@ import Sidebar from "components/common/sidebar";
 import { useSelector } from "react-redux";
 import { selectActiveMenu } from "./slice/appSlice";
 
-function Dashboard() {
+const getSidebarWidth = (activeMenu) => {
+  switch (activeMenu) {
+    case "open":
+      return "w-72 fixed";
+    case "onlyIcon":
+      return "w-30";
+    default:
+      return "w-0";
+  }
+};
+
+const getMainContentMargin = (activeMenu) => {
+  switch (activeMenu) {
+    case "open":
+      return "md:ml-72";
+    case "onlyIcon":
+      return "md:ml-2";
+    default:
+      return "flex-2";
+  }
+};
+
+const Dashboard = () => {
   const activeMenu = useSelector(selectActiveMenu);
+  const sidebarWidth = getSidebarWidth(activeMenu);
+  const mainContentMargin = getMainContentMargin(activeMenu);
 
   return (
     <div>
       <div className="flex relative dark:bg-main-dark-bg bg-gray-100">
-        {activeMenu === "open" ? (
-          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-            <Sidebar />
-          </div>
-        ) : activeMenu === "onlyIcon" ? (
-          <div className="w-30 dark:bg-secondary-dark-bg bg-white">
-            <Sidebar />
-          </div>
-        ) : (
-          <div className="w-0 dark:bg-secondary-dark-bg"></div>
-        )}
+        <div className={`${sidebarWidth} sidebar dark:bg-secondary-dark-bg bg-white`}>
+          <Sidebar />
+        </div>
 
-        <div
-          className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
-            activeMenu === "open"
-              ? "md:ml-72"
-              : activeMenu === "onlyIcon"
-              ? "md:ml-2"
-              : "flex-2"
-          }`}
-        >
+        <div className={`${mainContentMargin} dark:bg-main-dark-bg bg-main-bg min-h-screen w-full`}>
           <div className="bg-main-bg dark:bg-main-dark-bg navbar w-full">
             <Navbar />
           </div>
@@ -41,6 +49,6 @@ function Dashboard() {
       </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
