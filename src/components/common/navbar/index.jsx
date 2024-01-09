c451a9fd61f8fc357bd590a081f8727e9aa6c1a2
Pankajan Satkunam <pankajan05@gmail.com>
2024-01-09 11:08:07 +0530

    add prettier config

--- src/components/common/navbar/index.jsx
+++ src/components/common/navbar/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Bars3Icon, ChevronDownIcon } from '@heroicons/react/24/outline'
-import avatar from "../../../assets/images/avatar.jpg"
+import React from 'react';
+import { Bars3Icon, ChevronDownIcon } from '@heroicons/react/24/outline';
+import avatar from '../../../assets/images/avatar.jpg';
 
-import { useDispatch, useSelector } from "react-redux";
-import {setActiveMenu, selectActiveMenu} from "../../../pages/dashboard/slice/appSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveMenu, selectActiveMenu } from '../../../pages/dashboard/slice/appSlice';
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -12,50 +12,38 @@ const Navbar = () => {
   const toggleActiveMenu = () => {
     let newActiveMenu;
 
-    if (activeMenu === "open") {
-      newActiveMenu = "onlyIcon";
-    } else if (activeMenu === "onlyIcon") {
-      newActiveMenu = "close";
+    if (activeMenu === 'open') {
+      newActiveMenu = 'onlyIcon';
+    } else if (activeMenu === 'onlyIcon') {
+      newActiveMenu = 'close';
     } else {
-      newActiveMenu = "open";
+      newActiveMenu = 'open';
     }
 
     dispatch(setActiveMenu(newActiveMenu));
   };
 
-
-
   return (
     <div className="flex justify-between p-2 md:mr-6 relative">
-
       <button
         type="button"
         className="relative text-xl rounded-full p-3 hover:bg-light-gray"
-        
-        onClick={toggleActiveMenu}
-
-      >
+        onClick={toggleActiveMenu}>
         {/* <span
           style={{ background: dotColor }}
           className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
         /> */}
-        <Bars3Icon className="h-6 w-6 cursor-pointer"/>
+        <Bars3Icon className="h-6 w-6 cursor-pointer" />
       </button>
 
       <div className="flex">
         <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg">
-          <img
-            className="rounded-full w-8 h-8"
-            src={avatar}
-            alt="user-profile"
-          />
+          <img className="rounded-full w-8 h-8" src={avatar} alt="user-profile" />
           <p>
-            <span className="text-gray-400 text-14">Hi,</span>{" "}
-            <span className="text-gray-400 font-bold ml-1 text-14">
-              Michael
-            </span>
+            <span className="text-gray-400 text-14">Hi,</span>{' '}
+            <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
           </p>
-          <ChevronDownIcon className="h-6 w-6 text-14 text-gray-400"/>
+          <ChevronDownIcon className="h-6 w-6 text-14 text-gray-400" />
         </div>
       </div>
     </div>
@@ -63,11 +51,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-
-
-
-
-
-
