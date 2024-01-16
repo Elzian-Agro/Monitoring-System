df1970008740974475f7b2486a0e195e3d54e9e3
Musarraf <muhammedmusarraf@gmail.com>
2024-01-17 02:54:18 +0530

    Change the usestate to redux toolkit in the profile and notification

--- src/pages/dashboard/components/common/notification-set/index.jsx
+++ src/pages/dashboard/components/common/notification-set/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { XCircleIcon } from '@heroicons/react/24/outline';
 const Notification = () => {
   return (
-    <div className="nav-item absolute right-5 md:right-40 top-16 bg-white p-8 rounded-lg w-96">
-      <div className="flex justify-between items-center">
-        <div className="flex gap-3">
-          <p className="font-semibold text-lg">Notifications</p>
+    <div className='nav-item absolute right-5 md:right-40 top-16 bg-white p-8 rounded-lg w-96'>
+      <div className='flex justify-between items-center'>
+        <div className='flex gap-3'>
+          <p className='font-semibold text-lg'>Notifications</p>
         </div>
+        <button>
+          <XCircleIcon className='h-6 w-6' />
+        </button>
       </div>
     </div>
   );
--- src/pages/dashboard/slice/dashboardLayoutSlice.js
+++ src/pages/dashboard/slice/dashboardLayoutSlice.js
@@ -2,6 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   activeMenu: 'open',
+  isProfileOpen: false,
+  isNotificationOpen: false,
 };
 
 const dashboardLayoutSlice = createSlice({
@@ -11,10 +13,19 @@ const dashboardLayoutSlice = createSlice({
     setActiveMenu: (state, action) => {
       state.activeMenu = action.payload;
     },
+    setProfileOpen: (state, action) => {
+      state.isProfileOpen = action.payload;
+    },
+    setNotificationOpen: (state, action) => {
+      state.isNotificationOpen = action.payload;
+    }
   },
 });
 
-export const { setActiveMenu } = dashboardLayoutSlice.actions;
+export const { setActiveMenu, setProfileOpen, setNotificationOpen } = dashboardLayoutSlice.actions;
+
 export const selectActiveMenu = (state) => state.dashboardLayout.activeMenu;
+export const selectProfileOpen = (state) => state.dashboardLayout.isProfileOpen;
+export const selectNotificationOpen = (state) => state.dashboardLayout.isNotificationOpen;
 
 export default dashboardLayoutSlice.reducer;
