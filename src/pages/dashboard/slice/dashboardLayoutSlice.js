e6a420bc59d86e6687157295b55ede9ab771d44a
Musarraf <muhammedmusarraf@gmail.com>
2024-01-17 06:19:24 +0530

    Basic light and dark mode working

--- src/pages/dashboard/slice/dashboardLayoutSlice.js
+++ src/pages/dashboard/slice/dashboardLayoutSlice.js
@@ -4,6 +4,7 @@ const initialState = {
   activeMenu: 'open',
   isProfileOpen: false,
   isNotificationOpen: false,
+  theme: '',
 };
 
 const dashboardLayoutSlice = createSlice({
@@ -18,14 +19,18 @@ const dashboardLayoutSlice = createSlice({
     },
     setNotificationOpen: (state, action) => {
       state.isNotificationOpen = action.payload;
-    }
+    },
+    setTheme: (state, action) => {
+      state.theme = action.payload;
+    },
   },
 });
 
-export const { setActiveMenu, setProfileOpen, setNotificationOpen } = dashboardLayoutSlice.actions;
+export const { setActiveMenu, setProfileOpen, setNotificationOpen, setTheme } = dashboardLayoutSlice.actions;
 
 export const selectActiveMenu = (state) => state.dashboardLayout.activeMenu;
 export const selectProfileOpen = (state) => state.dashboardLayout.isProfileOpen;
 export const selectNotificationOpen = (state) => state.dashboardLayout.isNotificationOpen;
+export const selectTheme = (state) => state.dashboardLayout.theme;
 
 export default dashboardLayoutSlice.reducer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         zenkaku: ["'Zen Kaku Gothic New'", "sans-serif"],
+      },
+      backgroundColor: {
+        'main-bg': '#FAFBFB',
+        'main-dark-bg': '#20232A',
+        'secondary-dark-bg': '#33373E',
+        'light-gray': '#F7F7F7',
       },
       screens: {
         xs: "450px",
