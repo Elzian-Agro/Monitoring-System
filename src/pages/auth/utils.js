34bd73b7e4f3902645223eb3a905d6e51172fea1
Abdullah Anaz <abdullahanaz@yahoo.com>
2024-01-18 10:03:56 +0530

    Fix: contstants and env

--- src/pages/auth/utils.js
+++ src/pages/auth/utils.js
@@ -1,15 +1,15 @@
+import { authRegex } from "../../constants";
+
 // Email validation function
 export const isValidEmail = (email) => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
+  return authRegex.email.test(email);
 };
 
 // Password validation function
 export const isValidPassword = (password) => {
   // Validate password for minimum 8 characters, at least one uppercase letter,
   // one lowercase letter, one number, and one special character
-  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  return passwordRegex.test(password);
+  return authRegex.password.test(password);
 };
 
 // Encryption
