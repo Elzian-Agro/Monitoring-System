2f8a2805521afa37b81a7e76416ec34137d0b5ed
Musarraf <muhammedmusarraf@gmail.com>
2024-01-26 22:40:17 +0530

    sussesfully get the datqas from the server and render the notifications

--- src/pages/dashboard/components/common/notification/index.jsx
+++ src/pages/dashboard/components/common/notification/index.jsx
@@ -7,26 +7,46 @@ import { useDispatch } from 'react-redux';
 
 const Notification = () => {
   const dispatch = useDispatch();
-  const [allNotifications, setAllNotoficatins] = useState([
-    { desc: 'This is the First Notification', date: '2024/01/01', time: '08:00 AM', read: false },
-    {
-      desc: 'This is the Second Notification, its length is biiger than the first one.',
-      date: '2024/01/02',
-      time: '09:00 AM',
-      read: false,
-    },
-  ]);
+  const [allNotifications, setAllNotifications] = useState([]);
 
   useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_BASE_URL}/notification/fetch-notification`, {
-        params: {
-          userId: '6599ae73acebfda083c2f1b0',
-        },
+      .post(`${process.env.REACT_APP_BASE_URL}/notification/fetch-notification`, {
+        userId: '6599ae73acebfda083c2f1b0',
       })
       .then((response) => {
-        console.log(response.data);
-        // Update state or dispatch an action based on the response if needed
+        const datas = response.data.result;
+
+        const notifications = datas.map((data) => {
+          // get the Date from the timestampString
+          const timestampString = data.dateTime;
+          const timestamp = new Date(timestampString);
+
+          // Extracting date components
+          const year = timestamp.getUTCFullYear();
+          const month = (timestamp.getUTCMonth() + 1).toString().padStart(2, '0');
+          const day = timestamp.getUTCDate().toString().padStart(2, '0');
+
+          // Extracting time components
+          let  hours = timestamp.getUTCHours().toString().padStart(2, '0');
+          const minutes = timestamp.getUTCMinutes().toString().padStart(2, '0');
+          // Determining AM/PM
+          const ampm = hours >= 12 ? 'PM' : 'AM';
+          // Converting to 12-hour format
+          hours = hours % 12 || 12;
+
+          // Forming the date-only and timeonly  string
+          const dateOnly = `${year}-${month}-${day}`;
+          const timeOnly = `${hours}-${minutes} ${ampm}`;
+
+          return {
+            desc: data.notification,
+            date: dateOnly,
+            time: timeOnly,
+            read: false,
+          };
+        });
+        setAllNotifications(notifications);
       })
       .catch((error) => {
         console.log(error);
@@ -44,13 +64,13 @@ const Notification = () => {
   const deleteNotification = (index) => {
     const updatedNotifications = [...allNotifications];
     updatedNotifications.splice(index, 1);
-    setAllNotoficatins(updatedNotifications);
+    setAllNotifications(updatedNotifications);
   };
 
   const readNotification = (index) => {
     const updatedNotifications = [...allNotifications];
     updatedNotifications[index].read = true;
-    setAllNotoficatins(updatedNotifications);
+    setAllNotifications(updatedNotifications);
   };
 
   return (
