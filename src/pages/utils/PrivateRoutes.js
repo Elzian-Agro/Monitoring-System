a891bb61935d717d035857d16d958042cd174bbf
Musarraf <muhammedmusarraf@gmail.com>
2024-02-09 01:16:43 +0530

    Used verify endpoint to verify user in the Private route

--- src/pages/utils/PrivateRoutes.js
+++ src/pages/utils/PrivateRoutes.js
@@ -1,8 +1,24 @@
 import { Outlet, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import useAxios from 'hooks/useAxios';
 
 const PrivateRoutes = () => {
-  // Check if the token is present in localStorage
-  const isAuthenticated = !!localStorage.getItem('jwtAccessToken');
+  // TODO: Change the state to false once fix the bug
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const { send } = useAxios();
+
+  // TODO: need to fix = getting the verified from resposneData but not setting in the state immediately
+  useEffect(() => {
+    const verifyUser = async () => {
+      const responseData = await send({ endpoint: 'user/verify', method: 'POST' });
+      setIsAuthenticated(responseData?.verified);
+      // console.log(responseData?.verified);
+      // console.log(isAuthenticated);
+    };
+
+    verifyUser();
+    // eslint-disable-next-line
+  }, []);
 
   return isAuthenticated ? <Outlet /> : <Navigate to='/' />;
 };
