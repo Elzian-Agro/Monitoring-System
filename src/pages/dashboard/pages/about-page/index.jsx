3384e3b0b118dedb56114be81ae9a804fc3a3f66
tkdeshan <tkdeshan1103@gmail.com>
2024-02-23 02:01:50 +0530

    change styles in about page and add translations

--- src/pages/dashboard/pages/about-page/index.jsx
+++ src/pages/dashboard/pages/about-page/index.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import logo from 'assets/images/logo.png';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const AboutPage = () => {
-  return (
-    <div className='m-6 container place-items-center'>
-      <div className='bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md'>
-        <h2 className='text-4xl font-bold text-black dark:text-white text-center mb-4 pt-4'>ELZIAN AGRO</h2>
-        <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 pt-4 gap-6'>
-          <div className='col-span-3'>
-            <p className='text-gray-600 dark:text-gray-300 p-6 text-justify'>
-              ELZIAN AGRO provides smart agronomy solutions to support mass cultivation farmers to monitor and automate
-              their farmland. According to the latest report from the United Nations Intergovernmental Panel on Climate
-              Change, the global average temperature rise could reach or exceed 1.5 degrees Celsius in the next 20
-              years. That’s 10 years sooner than expected. This unprecedented extreme weather change and scarcity of
-              resources impact mass cultivation farmers. We offer customized monitoring information and automation
-              solutions through trending digital technologies to optimize our customers' harvest and productively
-              utilize their resources..
-            </p>
-          </div>
-          <div className='justify-center'>
-            <img src={logo} alt='Elzian Agro logo' />
-          </div>
-        </div>
+  const { t } = useTranslation();
 
-        <div className='text-center p-4 dark:text-white'>
-          <Link to='https://agro.elzian.com' target='_blank' rel='noreferrer'>
-            Go To Elzian Agro
+  return (
+    <div
+      className={`flex justify-center items-center bg-gradient-to-br from-green-500 to-lime-500 dark:from-gray-900 dark:to-gray-500 rounded-xl shadow-md px-8 py-20 mx-6 my-4`}>
+      <div className='bg-white bg-opacity-30 backdrop-blur-lg border border-gray-300 shadow-lg rounded-xl flex justify-center items-center p-4'>
+        <div className='flex flex-col gap-6'>
+          <h2 className='text-4xl font-bold text-black dark:text-white text-center'>ELZIAN AGRO</h2>
+          <div className='flex flex-col lg:flex-row lg:gap-5 items-center'>
+            <p className='text-black dark:text-white p-6 text-justify lg:w-2/3'>{t('About EA')}</p>
+            <div className='flex justify-center items-center lg:w-80'>
+              <img src={logo} alt='Elzian Agro logo' className='w-80' />
+            </div>
+          </div>
+          <Link
+            to='https://agro.elzian.com'
+            target='_blank'
+            rel='noreferrer'
+            className='text-center text-blue-500 dark:text-blue-800 hover:text-lg transition duration-300'>
+            {t('Go To Elzian Agro')}
           </Link>
         </div>
       </div>
