2618cc0e6a4cf55864b0a6fcbe88affe95663ab2
Pankajan Satkunam <pankajan05@gmail.com>
2024-02-26 00:17:06 +0530

    remove the green bg

--- src/pages/dashboard/pages/about-page/index.jsx
+++ src/pages/dashboard/pages/about-page/index.jsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
   const { t } = useTranslation();
 
   return (
-    <div className='flex justify-center bg-gradient-to-br from-green-500 to-lime-500 dark:from-gray-900 dark:to-gray-500 rounded-xl shadow-md p-8 sm:p-10 md:p-16 lg:p-24 mx-6 min-h-screen'>
+    <div className='flex justify-center dark:from-gray-900 dark:to-gray-500 rounded-xl shadow-md p-8 sm:p-10 md:p-16 lg:p-24 mx-6 min-h-screen'>
       <div className='bg-white bg-opacity-30 border border-gray-100 shadow-md rounded-md shadow-black/5 flex justify-center items-center p-4'>
         <div className='flex flex-col gap-5 sm:gap-8 md:gap-12 lg:gap-8'>
           <h2 className='text-4xl font-bold text-black dark:text-white text-center'>ELZIAN AGRO</h2>
