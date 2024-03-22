721290b27b9540643354064a49efafe5953193ba
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-03-22 11:51:18 +0530

    change toSort to sort

--- src/pages/dashboard/utils/generatePassword.js
+++ src/pages/dashboard/utils/generatePassword.js
@@ -21,5 +21,5 @@ export const generatePassword = (length = 12) => {
   }
 
   // Shuffle characters for randomness
-  return password.toSorted(() => Math.random() - 0.5).join('');
+  return password.sort(() => Math.random() - 0.5).join('');
 };
