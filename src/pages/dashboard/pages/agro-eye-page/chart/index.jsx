381e1e8cbaff077830b399516ec72fab65a40085
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-04-03 17:25:28 +0530

    modify chart with actual sensor data

--- src/pages/dashboard/pages/agro-eye-page/chart/index.jsx
+++ src/pages/dashboard/pages/agro-eye-page/chart/index.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+import highchartsStock from 'highcharts/modules/stock';
+import highchartsAccessibility from 'highcharts/modules/accessibility';
 import exportingInit from 'highcharts/modules/exporting';
 import offlineExportingInit from 'highcharts/modules/offline-exporting';
 import { useSelector } from 'react-redux';
 import { selectTheme } from 'pages/dashboard/slice/dashboardLayoutSlice';
 
+highchartsStock(Highcharts);
+highchartsAccessibility(Highcharts);
 exportingInit(Highcharts);
 offlineExportingInit(Highcharts);
 
@@ -13,102 +17,102 @@ const Chart = ({ widget }) => {
   const [chartOptions, setChartOptions] = useState(null);
   const currentMode = useSelector(selectTheme);
 
+  const getColor = (darkColor, lightColor) => (currentMode === 'Dark' ? darkColor : lightColor);
+
   useEffect(() => {
-    const getColor = (darkColor, lightColor) => (currentMode === 'Dark' ? darkColor : lightColor);
+    const sensorSeries = widget.devices.flatMap((device) => {
+      const deviceId = device.deviceId;
+      const factors = device.factors;
+      const series = factors.map((factor) => {
+        const seriesData = widget.sensorData
+          .find((sensorData) => sensorData[deviceId])
+          [deviceId].map((entry) => {
+            return {
+              date: new Date(entry.dateTime).toLocaleString('en-US', {
+                timeZone: 'Asia/Colombo',
+                dateStyle: 'long',
+                timeStyle: 'short',
+              }),
+              values: entry[factor],
+            };
+          });
 
-    const generateAxisTime = () => {
-      const startDate = new Date(widget.startDate);
-      const endDate = new Date(widget.endDate);
-      const timeGap = widget.timeGap.toLowerCase();
-      const axisTime = [];
+        const timeSeriesData = Array.from({ length: widget.axisTime.length }).fill(null);
 
-      let currentDate = startDate;
-      while (currentDate <= endDate) {
-        axisTime.push(currentDate.toLocaleString());
-        currentDate = new Date(currentDate.getTime() + getTimeGapInMilliseconds(timeGap));
-      }
-
-      return axisTime;
-    };
-
-    const getTimeGapInMilliseconds = (timeGap) => {
-      const timeGapMap = {
-        '2min': 120000,
-        '5min': 300000,
-        '10min': 600000,
-      };
-      return timeGapMap[timeGap];
-    };
-
-    const axisTime = generateAxisTime();
-
-    const sensorData = widget.devices
-      .map((device) => {
-        const deviceId = device.deviceId;
-        const factors = device.factors;
-        const series = factors.map((factor) => {
-          const data = widget.sensorData
-            .find((sensorData) => sensorData[deviceId])
-            [deviceId].map((entry) => entry[factor]);
-          return {
-            name: deviceId + ' ' + factor,
-            data,
-          };
+        seriesData.forEach((data) => {
+          const timeIndex = widget.axisTime.findIndex((time) => time === data.date);
+          if (timeIndex !== -1) {
+            timeSeriesData[timeIndex] = data.values;
+          }
         });
-        return series;
-      })
-      .flat();
+
+        return {
+          name: deviceId + ' ' + factor,
+          data: timeSeriesData,
+        };
+      });
+
+      return series;
+    });
 
     const chartConfig = {
       chart: {
         type: widget.chartType,
+        backgroundColor: getColor('#414345', '#ffffff'),
         scrollablePlotArea: {
           minWidth: 500,
           scrollPositionX: 1,
         },
-        backgroundColor: getColor('#414345', 'white'),
       },
       title: {
         text: widget.name,
         style: {
-          color: getColor('white', 'black'),
+          color: getColor('#ffffff', '#00000'),
         },
       },
       xAxis: {
-        categories: axisTime,
-        labels: {
+        categories: widget.axisTime,
+        title: {
+          text: null,
           style: {
-            color: getColor('white', 'black'),
+            color: getColor('#ffffff', '#00000'),
           },
         },
+        labels: {
+          style: {
+            color: getColor('#ffffff', '#00000'),
+          },
+        },
+        min: 0,
+        max: 4,
+        scrollbar: {
+          enabled: true,
+        },
+        tickLength: 0,
       },
       yAxis: {
         title: {
-          text: '',
+          text: null,
           style: {
-            color: getColor('white', 'black'),
+            color: getColor('#ffffff', '#00000'),
           },
         },
         labels: {
           style: {
-            color: getColor('white', 'black'),
+            color: getColor('#ffffff', '#00000'),
           },
         },
       },
-      legend: {
-        itemStyle: {
-          color: getColor('white', 'black'),
-        },
-      },
-      series: sensorData,
+      series: sensorSeries,
     };
 
     setChartOptions(chartConfig);
+    // eslint-disable-next-line
   }, [widget, currentMode]);
 
   return (
     <>
-      <div className='bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-800 p-1 w-full'>
+      <div className='bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-800 p-1 '>
         {chartOptions && <HighchartsReact highcharts={Highcharts} options={chartOptions} />}
       </div>
     </>
