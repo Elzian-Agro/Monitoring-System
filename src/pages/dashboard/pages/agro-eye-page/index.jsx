c8d096c693f1fd317881dfce283f27a6e45f6da9
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-05-20 12:05:00 +0530

    fix dark mode issue

--- src/pages/dashboard/pages/agro-eye-page/index.jsx
+++ src/pages/dashboard/pages/agro-eye-page/index.jsx
@@ -128,7 +128,7 @@ const AgroEye = () => {
 
                       return (
                         <div key={id}>
-                          <div className='bg-white dark:bg-gray-800 rounded-md border border-gray-200 dark:border-gray-800 shadow-md shadow-black/5 p-1 w-full'>
+                          <div className='bg-white dark:bg-secondary-dark-bg rounded-md border border-gray-100 dark:border-gray-600 shadow-md shadow-black/5 p-1 w-full'>
                             <div className='flex justify-end gap-2 mr-2 py-2'>
                               <IconButton
                                 testid='update-button'
