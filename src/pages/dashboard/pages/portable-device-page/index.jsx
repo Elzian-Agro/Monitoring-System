56d2f42fc2c1843757cc0eb7e525485aef2ee2d0
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-07-16 10:15:45 +0530

    add portable device page

---
+++ src/pages/dashboard/pages/portable-device-page/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PortableDevice = () => {
+  return (
+    <div className='mx-5 mt-2'>
+      <div>Portable Device</div>
+    </div>
+  );
+};
+
+export default PortableDevice;
