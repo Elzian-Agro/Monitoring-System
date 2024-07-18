576aa63bc68212f943769c458e90def705b0bf7f
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-07-18 19:47:24 +0530

    portable device connect wiith backend and add translation

--- src/pages/dashboard/pages/portable-device-page/index.jsx
+++ src/pages/dashboard/pages/portable-device-page/index.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import useFetch from 'hooks/useFetch';
 
 const PortableDevice = () => {
+  const {
+    response: soilElementsData,
+    recall,
+    isLoading,
+  } = useFetch({
+    endpoint: 'portable-device',
+    method: 'GET',
+    call: 1,
+    requestBody: {},
+    dependency: [],
+  });
+
+  // TO DO: Complete page
+
   return (
     <div className='mx-5 mt-2'>
       <div>Portable Device</div>
